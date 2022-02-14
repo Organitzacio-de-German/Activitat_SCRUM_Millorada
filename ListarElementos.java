3708f432b4806e788c07162ea706821350dbb266
Pablo Conca <pabloconcasanchez@gmail.com>
2022-02-14 18:46:27 +0100

    ADD ListarElementos y MostrarMenu PabloConca

---
+++ ListarElementos.java
@@ -0,0 +1,22 @@
+package Activitat_SCRUM_Millorada;
+
+import java.util.Scanner;
+
+public class ListarElementos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String array [] = new String [20];
+
+        listarElementos(array);
+        char eixir = scanner.next().charAt(0);
+        if (eixir == 'q'){
+            return;
+        }
+    }
+    public static void listarElementos(String arr[]){
+        for (int i = 0; i <arr.length ; i++) {
+            if (arr[i] != null)
+                System.out.println(arr[i]);
+        }
+    }
+}
